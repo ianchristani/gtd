a66e64582d28f75c23329a9b6d39735e77ee3c94
ianchristani <ianchristani@gmail.com>
2022-09-29 12:16:29 -0300

    some routes to fix

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name='crud'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('list_products/', views.list_products, name='list_products'),
     path('new/', views.create_product, name='create_product'),
     path('update/<int:id>', views.update_product, name='update_product'),
--- products/views.py
+++ products/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render, redirect
 from .models import products_model
 from .forms import products_form
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
+# pagina inicial de usuarios logados no CRUD
 def list_products(request):
     products_list = products_model.objects.all()
     return render(request, 'products.html', {'products':products_list})
 
+# pagina de inclusao de dados
 def create_product(request):
     form_instance = products_form(request.POST or None)
 
@@ -19,6 +17,7 @@ def create_product(request):
 
     return render(request, 'products-form.html', {'product_form':form_instance})
 
+# pagina de edicao de dados
 def update_product(request, id):
     selected_product = products_model.objects.get(id=id)
     form_instance = products_form(request.POST or None, instance=selected_product)
@@ -29,6 +28,7 @@ def update_product(request, id):
 
     return render(request, 'products-form.html', {'product_form':form_instance, 'id':selected_product})
 
+# pagina de exclusao de dados
 def delete_product(request, id):
     selected_product = products_model.objects.get(id=id)
     
