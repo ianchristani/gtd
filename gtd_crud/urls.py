d6e4ca9ae0fe387ec975bed0779c182ccc964e06
ianchristani <ianchristani@gmail.com>
2022-10-03 19:51:10 -0300

    product image exibition added to the forms

--- gtd_crud/urls.py
+++ gtd_crud/urls.py
@@ -13,19 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
-'''
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
-    re_path(r'^accounts/', include('usersapp.urls')),
-]
-'''
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usersapp.urls')),
     path('crud/', include('products.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class products_model(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     amount = models.IntegerField()
+    pic = models.ImageField(null=True, upload_to='media/')
 
     def __str__(self):
         return self.name
